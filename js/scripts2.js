d78a83f5b36f3aa87e75eb422c273380bf0951f5
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-19 16:49:20 -0700

    add write sentence alert with first and last characters capitalized

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,1 +1,12 @@
-$(document).ready(function() {
+const sentence = prompt("Please enter a sentence");
+const first = sentence.charAt(0).toUpperCase() + sentence.slice(1, -1);
+const middle = sentence.slice(0,-1);
+const last = sentence.slice(-1).toUpperCase();
+
+function upperLower() {
+    return first + last 
+}
+
+let final = upperLower();
+  
+alert(final);
